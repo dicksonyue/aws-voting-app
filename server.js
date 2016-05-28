94722f3e39846d7c1789b22b4424633e8c8bab94
Dickson Yue <dicksonyue@gmail.com>
2016-05-28 20:07:06 +0800

    added instance id

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 
 // Constants
 const PORT = 8080;
 
+var options = {
+  host: '169.254.169.254',
+  port: 80,
+  path: '/latest/meta-data/instance-id',
+  method: 'GET'
+};
+
+
+
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  try {
+    http.request(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+        res.send('instance-id: ' + chunk);
+        res.send();
+      });
+    }).on('error', function(e) {
+        console.log("error: " + e.message);
+        res.send( "error: " + e.message);
+
+    }).end();
+  } catch (err) {
+    console.log(err);
+    res.send('Hello world\n');
+  }
 });
 
 app.listen(PORT);
